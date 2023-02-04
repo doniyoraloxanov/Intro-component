2477e650b8a83b6bae97318b7682eaad39cce12f
doniyoraloxanov <doniyoraloxanov@gmail.com>
2023-02-04 22:25:06 +0500

    README file ready

---
+++ README.md
@@ -0,0 +1,34 @@
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+
+### Desktop version
+
+![Desktop vesrion 1](./images/desktop-one.png)
+![Desktop vesrion 2](./images/desktop-two.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- JavaScript Dom
+- Form validations
+
+## Author
+
+- Frontend Mentor - [@aloxanovme](https://www.frontendmentor.io/profile/aloxanovme)
+- Linkedin - [Doniyor aloxanov](https://www.linkedin.com/in/doniyoraloxanov/)
+- Gmail - doniyoraloxanov@gmail.com
---
+++ script.js
@@ -0,0 +1,66 @@
+const form = document.getElementById("form");
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // Get values from the input
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (firstNameValue === "") {
+    setErrorFor(firstName, "First Name cannot be empty");
+  } else {
+    setSuccessFor(firstName);
+  }
+
+  if (lastNameValue === "") {
+    setErrorFor(lastName, "Last Name cannot be empty");
+  } else {
+    setSuccessFor(lastName);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Email is not valid");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be empty");
+  } else {
+    setSuccessFor(password);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement; // .form-control
+  const small = formControl.querySelector("small");
+
+  // Add error message inside small
+  small.innerText = message;
+
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
